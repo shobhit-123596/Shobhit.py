2ecc244448937cd852f58a2adc848fa3bcb4c5c6
Shobhit Pathak <pathakshobhit139@gmail.com>
2025-08-17 23:48:34 +0530

    Add files via upload

---
+++ length.py
@@ -0,0 +1,12 @@
+str1 = "apna"
+len1 = len(str1)
+print(len1)
+
+str2 = "college"
+len2 = len(str2)
+print(len2)
+final_str = str1 +" " + str2
+print(final_str)
+print(len(final_str))
+str = "shobhit"
+print(str[0:7])
---
+++ licence.py
@@ -0,0 +1,7 @@
+str = "i am studying python from ApnaCollege"
+print(str.count("o"))
+str = "Hi, $Iam the $ symbal $99.99"
+print(str.count("$"))
+age = 23
+if(age >= 19):
+    print("can vote or apply fo rthe license")
