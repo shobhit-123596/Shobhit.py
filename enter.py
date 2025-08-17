41a576a14d5a1bc50104e238f49b44719fcf92d9
Shobhit Pathak <pathakshobhit139@gmail.com>
2025-08-17 23:44:55 +0530

    Add files via upload

---
+++ enter.py
@@ -0,0 +1,42 @@
+name = ("Shobhit")
+age = 18
+price = 23.45
+print(name)
+
+print(type(name))
+print(type(age))
+print(type(price))
+old = False
+a = None 
+print(type(old))
+print(type(a))
+a = 67
+b = 65
+sum = a + b
+print(sum)
+a = 9
+b = 56
+print(a == b)
+print(a !=b)
+num = 10
+num = num + 10
+print("num :", num)
+num = 10 
+num **= 5
+print("num :", num)
+val1 = True
+val2 = False
+print("and operators:",val1 and val2)
+print("OR operator:", (a == b) or (a > b))
+name = input("enter your name:")
+print("Welcome",name)
+val = input("enter some value:")
+print(type(val), val)
+val = int(input("enter some value:"))
+print(type(val),val)
+int("5")
+val = int(input("enter some value:"))
+print(type(val),val)
+first = int(input("enter first:"))
+second = int(input("enter second:"))
+print("sum =", first + second)
