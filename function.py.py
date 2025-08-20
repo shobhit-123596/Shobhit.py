6a3f364121d22b99bebd788db667973dbcb26a06
Shobhit Pathak <pathakshobhit139@gmail.com>
2025-08-20 23:03:04 +0530

    Add files via upload

---
+++ function.py.py
@@ -0,0 +1,8 @@
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+num = int(input("Enter a number: "))
+result = check_even_odd(num)
+print(f"The number {num} is {result}.")
